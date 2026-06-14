a94b9aab519ca2e6e4a6378e311aa670e3ad7b23
mohammadmehdi <mhmdmehdidalvandi@gmail.com>
2026-06-14 15:45:04 +0330

    fixed package and server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors'
 import { connectToDB } from './config/db';
 
 
@@ -7,6 +11,14 @@ const app = express();
 
 dotenv.config();
 
+// Middleware
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors({credentials:true ,  origin:'http://localhost:3000'}));
 
 // Connection To Database
 connectToDB();
