cd3287247358c0ba1dda98666b1985730922b654
mohammadmehdi <mhmdmehdidalvandi@gmail.com>
2026-06-17 14:58:44 +0330

    project.controller.ts created

---
+++ src/controller/Project.controller.ts
@@ -0,0 +1,98 @@
+import { projectServices } from "../services/Project.services";
+import {Request , Response} from 'express'
+
+export const projectController = {
+    async create(req:Request , res:Response){
+        try{
+            const data = req.body;
+            const img = req.file?.path;
+            if(!img){
+                return res.status(400).json({
+                    status:'Unsuccessful',
+                    message:'Image is required'
+                });
+            }
+            const project = await projectServices.createProject({
+                ...data,
+                image:img
+            });
+
+            res.status(201).json({
+                status:'Successful',
+                message:'Project created Successfully ✅',
+                data:project
+            })
+        }catch(error:any){
+            res.status( error.status || 500).json({
+                status:'Unsuccessful',
+                message:error.message || 'Internal Server Error'
+            })
+        }
+    },
+    async update(req:Request , res:Response){
+        try{
+             const id = String(req.params.id)
+            const updateData ={
+                ...req.body,
+                ...(req.file && {image:req.file.path})
+            }
+            const project = await projectServices.updateProjectById(id, updateData)
+            res.status(200).json({
+                status:'Unsuccessful',
+                message:'Project updated successfully ✅',
+                data:project
+            })
+        }catch(error:any){
+            res.status(error.status || 500).json({
+                status:'Unsuccessful',
+                message:error.message || 'Internal Server Error',
+            })
+        }
+    },
+    async delete(req:Request , res:Response){
+        try{
+            const id = String(req.params.id)
+             await projectServices.deleteProjectById(id);
+            res.status(200).json({
+                status:'Successful',
+                message:'Delete project successfully ✅'
+            })
+        }catch(error:any){
+            res.status( error.status || 500).json({
+                status:'Unsuccessful',
+                message:error.message || 'Internal Server Error'
+            })
+        }
+    },
+    async getById(req:Request , res:Response){
+        try{    
+             const id = String(req.params.id)
+            const project = await projectServices.getProjectById(id);
+            res.status(200).json({
+                status:'Successful',
+                message:'Get project by ID successfully ✅',
+                data:project
+            })
+        }catch(error:any){
+            res.status( error.status || 500).json({
+                status:'Unsuccessful',
+                message:error.message || 'Internal Server Error'
+            })
+        }
+    },
+    async getAllProjects(req:Request , res:Response){
+        try{
+            const projects = await projectServices.getAllProject();
+            res.status(200).json({
+                status:'Successful',
+                message:"Get all projects successfully ✅",
+                data:projects
+            })
+        }catch(error:any){
+            res.status( error.status || 500).json({
+                status:'Unsuccessful',
+                message:error.message || 'Internal Server Error'
+            })            
+        }
+    }
+}
