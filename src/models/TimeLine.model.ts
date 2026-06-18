b80ad1ff167a742cac30f94737b409217c16ae2f
mohammadmehdi <mhmdmehdidalvandi@gmail.com>
2026-06-18 15:56:03 +0330

    created TimeLineModel.ts

---
+++ src/models/TimeLine.model.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+export const TimeLineSchema = new mongoose.Schema({
+    year:{
+        type:Number,
+        required:[true ,'Year is required'],
+        min:[2020, 'Year too old'],
+    },
+    role:{
+        type:String,
+        required:[true , 'Role is required'],
+        trim:true,
+    },
+    company:{
+        type:String,
+        required:[true , 'Company is required'],
+        trim:true,
+    },
+    detail:{
+        type:String,
+        required:[true , 'Detail is required'],
+        trim:true,
+    },
+    achievement:{
+        type:[String],
+        required:[true ,'Achievement is required'],
+    },
+    isCurrent:{
+        type:Boolean,
+        default:false
+    },
+},{
+    timestamps:true
+});
+
+const TimeLineModel = mongoose.model('TimeLine' , TimeLineSchema);
+
+export default TimeLineModel
