66a0f760dfa1293f4522c16d07fc966d150452b6
mohammadmehdi <mhmdmehdidalvandi@gmail.com>
2026-06-21 15:27:07 +0330

    fixed server

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,7 @@ app.use(express.urlencoded({extended:true}))
 
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors({credentials:true ,  origin:'http://localhost:3000'}));
+app.use(cors({credentials:true ,  origin:'http://localhost:5173'}));
 
 // Connection To Database
 connectToDB();
