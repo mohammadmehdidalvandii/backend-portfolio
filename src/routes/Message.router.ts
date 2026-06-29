358154313021c2427e9864f2e7c55dd4a825820b
mohammadmehdi <mhmdmehdidalvandi@gmail.com>
2026-06-29 14:59:24 +0330

    fixed route message

--- src/routes/Message.router.ts
+++ src/routes/Message.router.ts
@@ -4,7 +4,7 @@ import authMiddleware from '../middleware/auth.middleware';
 
 const router  = express.Router();
 
-router.get('/' , messageController.getAll)
+router.get('/' , authMiddleware , messageController.getAll)
 router.get('/:id' , messageController.getById)
 
 router.post('/' , messageController.create)
