a537352b2b079d8b1c46dec0840eb348966a74f4
mohammadmehdi <mhmdmehdidalvandi@gmail.com>
2026-07-12 08:21:00 +0330

    fixed uploads img

--- src/controller/Certificate.controller.ts
+++ src/controller/Certificate.controller.ts
@@ -14,7 +14,7 @@ export const certificateController = {
             };
             const certificate = await certificateServices.createCertificate({
                 ...data,
-                image:`https://api.web-dalvandi.ir/uploads/${img}`
+                image:`https://protfolio-back.chbkn.dev/uploads/${img}`
             });
 
             res.status(201).json({
@@ -34,7 +34,7 @@ export const certificateController = {
             const id = String(req.params.id);
             const updateData = {
                 ...req.body,
-                ...(req?.file && {image:`https://protfolio-back.chbkn.run/uploads/${req.file?.filename}`})
+                ...(req?.file && {image:`https://protfolio-back.chbkn.dev/uploads/${req.file?.filename}`})
             }
 
             const certificate = await certificateServices.updateCertificateById(id , updateData);
--- src/controller/Project.controller.ts
+++ src/controller/Project.controller.ts
@@ -14,7 +14,7 @@ export const projectController = {
             }
             const project = await projectServices.createProject({
                 ...data,
-                image:`https://protfolio-back.chbkn.run/uploads/${img}`
+                image:`https://protfolio-back.chbkn.dev/uploads/${img}`
             });
 
             res.status(201).json({
@@ -35,7 +35,7 @@ export const projectController = {
             const updateData ={
                 ...req.body,
                 ...(req?.file &&
-                    {image:`https://protfolio-back.chbkn.run/uploads/${req.file?.filename}`}
+                    {image:`https://protfolio-back.chbkn.dev/uploads/${req.file?.filename}`}
                 )
             }
             const project = await projectServices.updateProjectById(id, updateData)
