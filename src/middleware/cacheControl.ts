77cb2eb503d02e139d500fd64d34e819fe1c1872
mohammadmehdi <mhmdmehdidalvandi@gmail.com>
2026-08-19 10:02:18 +0330

    created middleware cache

---
+++ src/middleware/cacheControl.ts
@@ -0,0 +1,17 @@
+import {Request , Response , NextFunction} from 'express';
+
+export const setCacheHeaders =(
+    maxAge: number,
+    isPrivate: boolean = false
+)=>{
+    return (_req:Request , res:Response , next:NextFunction)=>{
+        const visibility = isPrivate ? 'private' : 'public';
+        res.set('Cache-Control',`${visibility} ,  max-age=${maxAge}`)
+        next()
+    }
+}
+
+export const noCache = (_req:Response , res:Response , next:NextFunction)=>{
+    res.set('Cache-Control','no-cache , no-store ,  must-revalidate');
+    next()
+}
